2a52872c6234cd1df090b5b57a0bea4e897a3c2a
MahMouD-YUsUF-M <mahmoudyusuf@gmail.com>
2026-08-24 16:19:08 +0300

    add-builder-to-catogries-and-filters

--- app.js
+++ app.js
@@ -22,3 +22,52 @@ const filtersEl = document.getElementById('category-filters');
 const summaryEl = document.getElementById('summary');
 const listEl = document.getElementById('expense-list');
 
+async function start() {
+    loadingEl.hidden = false;
+    errorEl.hidden = true;
+    appEl.hidden = true;
+
+    try {
+        const[loadedCategories, loadedExpenses] = await Promise.all([
+            fetchCategories(),
+            fetchInitialExpenses()
+        ]);
+        categories = loadedCategories;
+        seedExpenses = loadedExpenses;
+        userExpenses = loadExpenses();
+
+        buildCategoryOptions();
+        buildCategoryFilters();
+        render();
+        
+    }
+    catch (error) {
+        console.error("Error loading data:", error);
+        errorEl.hidden = false;
+        loadingEl.hidden = true;
+    }
+}
+
+    function buildCategoryOptions() {
+        for (const category of categories) {
+            const optionEl = document.createElement('option');
+            opttionEl.value = category;
+            optionEl.textContent = category;
+            categorySelect.appendChild(optionEl);
+        }
+    }
+
+    function buildCategoryFilters() {
+        for (const category of categories) {
+            const buttonEl = document.createElement('button');
+            buttonEl.type = 'button';
+            buttonEl.textContent = category;
+            buttonEl.dataset.category = category;
+            filtersEl.appendChild(buttonEl);
+    }
+
+    function render() {
+        
+    }
+
+}
