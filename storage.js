2c0eadf660dd48405d35221f8eb03ea0861b7faa
MahMouD-YUsUF-M <mahmoudyusuf@gmail.com>
2026-08-24 14:32:24 +0300

    make storage js

--- storage.js
+++ storage.js
@@ -1,2 +1,23 @@
 const STORGE_KEY = "expense-tracker:user-expenses";
 
+export const saveExpenses = (expenses) => {
+    try{
+    localStorage.setItem(STORGE_KEY, JSON.stringify(expenses));
+    } catch (error) {
+        console.warn("Failed to save expenses to localStorage:", error);
+    }
+}
+
+export const loadExpenses = () => {
+    const expenses = localStorage.getItem(STORGE_KEY);
+    if(!expenses) return [];
+    try{
+        const parsedExpenses = JSON.parse(expenses);
+        return Array.isArray(parsedExpenses) ? parsedExpenses : [];
+    }
+    catch (error) {
+        console.warn("Failed to load expenses from localStorage:", error);
+        return [];
+    }
+
+}
